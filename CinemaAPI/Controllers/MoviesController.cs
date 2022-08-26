159169d2131fdd58f612e46ffd8cc534520b88a4
surajpradhank <surajkumarpradhan2016@gmail.com>
2022-08-26 17:55:36 +0530

    Add project files.

---
+++ CinemaAPI/Controllers/MoviesController.cs
@@ -0,0 +1,155 @@
+﻿using CinemaApi.Data;
+using CinemaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace CinemaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+
+        public MoviesController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: /api/movies/AllMovies?sort=desc&pageNumber=1&pageSize=10
+        //[Authorize]
+        [HttpGet("[action]")]
+        public IActionResult AllMovies(string sort, int? pageNumber, int? pageSize)
+        {
+            var currentPageNumber = pageNumber ?? 1;
+            var currentPageSize = pageSize ?? 5;
+
+            var movies = from movie in _dbContext.Movies
+                         select new
+                         {
+                             movie.Id,
+                             movie.Name,
+                             movie.Duration,
+                             movie.Language,
+                             movie.Rating,
+                             movie.Genre,
+                             movie.ImageUrl
+                         };
+
+            switch (sort)
+            {
+                case "desc":
+                    return Ok(movies.Skip((currentPageNumber - 1) * currentPageSize).Take(currentPageSize).OrderByDescending(m => m.Rating));
+
+                case "asc":
+                    return Ok(movies.Skip((currentPageNumber - 1) * currentPageSize).Take(currentPageSize).OrderBy(m => m.Rating));
+
+                default:
+                    return Ok(movies.Skip((currentPageNumber - 1) * currentPageSize).Take(currentPageSize));
+            }
+
+        }
+
+        // GET api/<MoviesController>/5
+        //[Authorize]
+        [HttpGet("[action]/{id}")]
+        public IActionResult MovieDetail(int id)
+        {
+            var movie = _dbContext.Movies.Find(id);
+            if (movie == null)
+                return NotFound("Records not found");
+
+            return Ok(movie);
+        }
+
+        // GET api/movies/FindMovies?movieName=Frozen
+        [HttpGet("[action]")]
+        public IActionResult FindMovies(string movieName)
+        {
+            var movies = from movie in _dbContext.Movies
+                         where movie.Name.ToLower().StartsWith(movieName.ToLower())
+
+                         select new
+                         {
+                             movie.Id,
+                             movie.Name,
+                             movie.ImageUrl
+                         };
+
+            return Ok(movies);
+        }
+
+
+        //[Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult Post([FromForm] Movie movieObj)
+        {
+            var guid = Guid.NewGuid();
+            var filePath = Path.Combine("wwwroot", guid + ".jpg");
+
+            if (movieObj.Image != null)
+            {
+                var fileStream = new FileStream(filePath, FileMode.Create);
+                movieObj.Image.CopyTo(fileStream);
+            }
+
+            movieObj.ImageUrl = filePath.Remove(0, 7);
+            _dbContext.Movies.Add(movieObj);
+            _dbContext.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<MoviesController>/5
+        //[Authorize(Roles = "Admin")]
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromForm] Movie movieObj)
+        {
+            var movie = _dbContext.Movies.Find(id);
+            if (movie == null)
+                return NotFound("Records not found");
+
+            var guid = Guid.NewGuid();
+            var filePath = Path.Combine("wwwroot", guid + ".jpg");
+
+            if (movieObj.Image != null)
+            {
+                var fileStream = new FileStream(filePath, FileMode.Create);
+                movieObj.Image.CopyTo(fileStream);
+                movie.ImageUrl = filePath.Remove(0, 7);
+            }
+
+            movie.Name = movieObj.Name;
+            movie.Language = movieObj.Language;
+            movie.Description = movieObj.Description;
+            movie.Language = movieObj.Language;
+            movie.Duration = movieObj.Duration;
+            movie.PlayingDate = movieObj.PlayingDate;
+            movie.PlayingTime = movieObj.PlayingTime;
+            movie.Rating = movieObj.Rating;
+            movie.Genre = movieObj.Genre;
+            movie.TrailorUrl = movieObj.TrailorUrl;
+            movie.TicketPrice = movieObj.TicketPrice;
+
+            _dbContext.SaveChanges();
+            return Ok("Record Updated successfully");
+        }
+
+        // DELETE api/<MoviesController>/5
+        //[Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var movie = _dbContext.Movies.Find(id);
+            if (movie == null)
+                return NotFound("Records not found");
+
+            _dbContext.Movies.Remove(movie);
+            _dbContext.SaveChanges();
+            return Ok("Record deleted successfully");
+        }
+    }
+}
---
+++ CinemaAPI/Controllers/UsersController.cs
@@ -0,0 +1,88 @@
+﻿using AuthenticationPlugin;
+using CinemaApi.Data;
+using CinemaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CinemaApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+        private IConfiguration _configuration;
+        private readonly AuthService _auth;
+
+        public UsersController(CinemaDbContext dbContext, IConfiguration configuration)
+        {
+            _dbContext = dbContext;
+            _configuration = configuration;
+            _auth = new AuthService(_configuration);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            var userWithSameEmail = _dbContext.Users.Where(x => x.Email == user.Email).SingleOrDefault();
+            if (userWithSameEmail != null)
+                return BadRequest("User with same email already exist");
+
+            var usreObj = new User
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = SecurePasswordHasherHelper.Hash(user.Password),
+                Role = "Users"
+            };
+
+            _dbContext.Users.Add(usreObj);
+            await _dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var userEmail = _dbContext.Users.FirstOrDefault(x => x.Email == user.Email);
+            if (userEmail == null)
+            {
+                return NotFound();
+            }
+
+            if (!SecurePasswordHasherHelper.Verify(user.Password, userEmail.Password))
+            {
+                return Unauthorized();
+            }
+
+            // Generate JWT 
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, userEmail.Email),
+                new Claim(ClaimTypes.Email , userEmail.Email),
+                new Claim(ClaimTypes.Role, userEmail.Role)
+            };
+
+            var token = _auth.GenerateAccessToken(claims);
+            return new ObjectResult(new
+            {
+                access_token = token.AccessToken,
+                expires_in = token.ExpiresIn,
+                token_type = token.TokenType,
+                creation_Time = token.ValidFrom,
+                expiration_Time = token.ValidTo,
+                user_id = userEmail.Id
+            });
+        }
+    }
+}
+
+
